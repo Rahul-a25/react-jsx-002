85adc09a82089c260672a454607e7d0e5cd8e67f
unknown <rahularora993676@gmail.com>
2023-01-11 01:38:12 -0800

    jsx02

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="heading">The Evolution of Airbnb's Frontend</h1>
+        <p>@spikebrehm</p>
+        <img
+          src="https://www.manageair.co.za/wp-content/uploads/2016/08/airbnb-logo.png"
+          className="logowala"
+          alt="logo"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
